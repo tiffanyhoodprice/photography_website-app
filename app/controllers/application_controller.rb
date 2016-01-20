7e4f6cb5f6377accee1832498b811f2ca7d32292
Tiffany Price <tiffany.hood@stanfordalumni.org>
2016-01-19 23:01:26 -0800

    adding custom user fields to be permitted in application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,5 +7,11 @@ class ApplicationController < ActionController::Base
     redirect_to "/" unless current_user && current_user.user_type == "admin"
   end
 
+  def configure_permitted_parameters
+     devise_parameter_sanitizer.for(:sign_in) { |u| u.permit(:username, :email, :first_name, :last_name) }
+     devise_parameter_sanitizer.for(:sign_up) { |u| u.permit(:username, :email, :password, :password_confirmation, :first_name, :last_name, :phone_number) }
+     devise_parameter_sanitizer.for(:account_update) { |u| u.permit(:username, :email, :password, :password_confirmation, :current_password, :first_name, :last_name, :phone_number) }
+  end
+
 
 end
