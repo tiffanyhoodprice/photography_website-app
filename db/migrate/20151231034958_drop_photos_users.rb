5e9931c34acafb7edcbd6a8a58cf9482344225e1
Tiffany Price <tiffany.hood@stanfordalumni.org>
2016-01-19 20:15:44 -0800

    dropped unnecessary migrations

--- db/migrate/20151231034958_drop_photos_users.rb
+++
@@ -1,5 +0,0 @@
-class DropPhotosUsers < ActiveRecord::Migration
-  def change
-     drop_table :photos_users
-  end
-end
--- db/migrate/20160113045230_drop_user_photos_table.rb
+++
@@ -1,5 +0,0 @@
-class DropUserPhotosTable < ActiveRecord::Migration
-  def change
-    drop_table :user_photos
-  end
-end
